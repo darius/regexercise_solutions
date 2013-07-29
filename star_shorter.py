de0c839f812f827ea29b3defc5446208ee87240d
Darius Bacon <darius@wry.me>
2013-07-28 23:44:38 -0400

    add star_shorter

--- star_shorter.py
+++ star_shorter.py
@@ -1,4 +1,6 @@
-# See also star_shorter.py.
+"""
+A variant of star.py with shorter but trickier code.
+"""
 
 def search(re, chars):
     """Given a regular expression and an iterator of chars, return True
@@ -16,34 +18,29 @@ def search(re, chars):
 
 def nullable(re):
     "Does re match the empty string?"
-    tag, r, s = re
-    if tag == 'empty':     return True
-    elif tag == 'literal': return False
-    elif tag == 'chain':   return nullable(r) and nullable(s)
-    elif tag == 'either':  return nullable(r) or nullable(s)
-    elif tag == 'star':    return True
-    else: assert False
+    return empty in after(None, re)
 
 def after(ch, re):
     """Imagine all strings starting with ch that re matches; return a list
     of regexes that among them match the remainders of those strings. (For
     example, if ch is 'c', and re matches 'x', 'ca', 'cat', and 'cow', and
     [q,r,s] is the result: that means q|r|s must match 'a', 'at', and 'ow'.)
-    This is called the Brzozowski derivative."""
+    ch may be None; in this case return a nonempty list if re matches the
+    empty string."""
     tag, r, s = re
-    if tag == 'empty':     return []
-    elif tag == 'literal': return [empty] if r == ch else []
+    if tag == 'literal':   return [empty] if r == ch else []
     elif tag == 'chain':
         dr_s = [chain(r_rest, s) for r_rest in after(ch, r)]
         return dr_s + after(ch, s) if nullable(r) else dr_s
     elif tag == 'either':  return after(ch, r) + after(ch, s)
-    elif tag == 'star':    return [chain(r_rest, re) for r_rest in after(ch, r)]
+    elif tag == 'star':    return ([empty] if None == ch else
+                                   [chain(r_rest, re) for r_rest in after(ch, r)])
     else: assert False
 
 # Regular-expression constructors; the re above is built by these.
-empty = ('empty', None, None)
 def literal(char): return ('literal', char, None)
 def chain(r, s):   return s if r is empty else ('chain', r, s)
 def either(r, s):  return ('either', r, s)
 def plus(r):       return chain(r, star(r))
 def star(r):       return ('star', r, None)
+empty = literal(None)
